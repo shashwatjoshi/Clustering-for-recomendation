866d98831c3166a527d0e9536a437ca6c7dd47b8
Shashat Joshi <shashwatjoshi@live.com>
2017-07-17 14:36:22 +0530

    Add files via upload

---
+++ MoviesC.R
@@ -0,0 +1,58 @@
+# Movie Recomendation
+
+
+#load the data into R
+movies = read.table("movieC.txt", header=FALSE, sep="|",quote="\"")
+
+str(movies)
+
+# Add column names
+colnames(movies) = c("ID", "Title", "ReleaseDate", "VideoReleaseDate", "IMDB", "Unknown", "Action", "Adventure", "Animation", "Childrens", "Comedy", "Crime", "Documentary", "Drama", "Fantasy", "FilmNoir", "Horror", "Musical", "Mystery", "Romance", "SciFi", "Thriller", "War", "Western")
+
+str(movies)
+
+# Remove unnecessary variables, will not be using these
+movies$ID = NULL
+movies$ReleaseDate = NULL
+movies$VideoReleaseDate = NULL
+movies$IMDB = NULL
+
+# Remove duplicates
+movies = unique(movies)
+
+# Take a look at our data again:
+str(movies)
+
+# Compute distances,not going to use the first title variable
+distances = dist(movies[2:20], method = "euclidean")
+
+# Hierarchical clustering
+clusterMovies = hclust(distances, method = "ward") 
+
+# Plot the dendrogram
+plot(clusterMovies)
+
+# Assign points to clusters,label each of the data points according to what cluster it belongs to 
+clusterGroups = cutree(clusterMovies, k = 10)
+
+# figure out what the clusters are like.
+
+# use the tapply function to compute the percentage of movies in each genre and cluster
+
+tapply(movies$Action, clusterGroups, mean)
+tapply(movies$Romance, clusterGroups, mean)
+
+# We can repeat this for each genre.The file ClusterA.ods has the results with the higest average highlighted for every genre
+
+
+# Find which cluster Men in Black is in.
+
+subset(movies, Title=="Men in Black (1997)")
+clusterGroups[257]
+
+# Create a new data set with just the movies from cluster 2
+cluster2 = subset(movies, clusterGroups==2)
+
+# Look at the first 10 titles in this cluster:
+cluster2$Title[1:10]
+
